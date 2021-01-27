87b5a2b1630573c5a992f5de960faac7e7304f62
Percy Luis Leon Lucas <percyleon107@gmail.com>
2021-01-27 12:21:31 -0500

    Acpturar datos del DNI por lectora OCR, tecnologia Blazor WebAssembly

---
+++ CapturarInfoOCR/Index.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace Aplicacion_Prueba.Pages
+{
+    public partial class Index
+    {
+        [Inject] protected IJSRuntime JS { get; set; }
+        protected string InputTextF = "";
+        IJSObjectReference Module;
+        ElementReference InputTextRef;
+
+        protected override async Task OnInitializedAsync() 
+        {
+            Module = await JS.InvokeAsync<IJSObjectReference>("import", "./js/Utilitario.js");
+            await Task.Delay(2000);
+            await Module.InvokeVoidAsync("Obtener", DotNetObjectReference.Create(this));
+        }
+
+        [JSInvokable]
+        public void MostrarInfo(string dato) 
+        {
+            InputTextRef.FocusAsync();
+            Console.WriteLine(dato);
+            StateHasChanged();
+        }
+    }
+}
---
+++ CapturarInfoOCR/Utilitario.js
@@ -0,0 +1,16 @@
+﻿var DotnetHelper;
+
+export function Obtener(dotnetHelper) {
+	document.onkeypress = mostrarInformacionTecla;
+	DotnetHelper = dotnetHelper;
+}	 
+
+function mostrarInformacionTecla(eventoObj) {
+	var caracter = String.fromCharCode(eventoObj.which);
+	DotnetHelper.invokeMethodAsync("MostrarInfo", caracter);
+}
+
+
+export function Liberar() {
+	document.onkeypress = null;
+}
